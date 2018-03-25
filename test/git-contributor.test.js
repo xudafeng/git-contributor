e3c64b39667fc8dec783c5dde884337a9778ce08
xudafeng <xudafeng@126.com>
2018-03-25 19:20:28 +0800

    update nyc

--- test/git-contributor.test.js
+++ test/git-contributor.test.js
@@ -2,9 +2,12 @@
 
 const assert = require('assert');
 
-const gitcontributor = require('..');
+const contributor = require('..');
 
 describe('test', () => {
-  it('should be ok', () => {
+  it('should be ok', (done) => {
+    contributor.getAuthor().then(d => {
+      done();
+    });
   });
 });
