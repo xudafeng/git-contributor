84bb1b2ddd3c16ab7c89456fa3dd3988c81c7a2d
Zhu Liang <zhuliangg11@gmail.com>
2019-01-25 13:39:11 +0800

    fix: typo and add test

--- test/git-contributor.test.js
+++ test/git-contributor.test.js
@@ -6,7 +6,9 @@ const contributor = require('..');
 
 describe('test', () => {
   it('should be ok', (done) => {
-    contributor.getAuthor().then(d => {
+    contributor.getAuthor().then(list => {
+      const res = contributor.genMarkDown(list);
+      assert.equal(/auto updated/.test(res.content), true);
       done();
     });
   });
